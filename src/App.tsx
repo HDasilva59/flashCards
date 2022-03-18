5d47698c7375a38b5f1bb2533993d8ddc5a72e24
Da silva <dasilva.hugo59200@gmail.com>
2022-03-18 11:14:38 +0100

    QCM cards working, add css on QCM, add a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,56 @@
-import React from 'react';
-import './App.css';
-import Checkcards from './Components/Checkcards';
-import FormCard from './Components/Formcards';
-import MentalCard from './Components/Mentalcard';
+import React from "react";
+import "./App.css";
+import Checkcards from "./Components/Checkcards";
+import FormCard from "./Components/Formcards";
+import MentalCard from "./Components/Mentalcard";
 
 const App = () => {
-const [whichCard, setWichCard] = React.useState(0)
+  const [whichCard, setWichCard] = React.useState(0);
 
-function WhichMentalCard (){
-  if (whichCard === 0) {
-    return <MentalCard/>
-  } else if (whichCard === 1){
-    return <FormCard/>
-  } else if (whichCard === 2) {
-    return <Checkcards/>
+  function WhichMentalCard() {
+    if (whichCard === 0) {
+      return <MentalCard />;
+    } else if (whichCard === 1) {
+      return <FormCard />;
+    } else if (whichCard === 2) {
+      return <Checkcards />;
+    }
   }
-}
 
   return (
     <div className="App">
-        <ul>
-          <li onClick={() => setWichCard(0)}>MentalCard</li>
-          <li onClick={() => setWichCard(1)}>MentalCard with form</li>
-          <li onClick={() => setWichCard(2)}>QCM</li>
-        </ul>
-        <p>{WhichMentalCard()}</p>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand">Fabien OliFlashCards</a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="true"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <li onClick={() => setWichCard(0)} className="nav-link">
+                MentalCard
+              </li>
+              <li onClick={() => setWichCard(1)} className="nav-link">
+                MentalCard with form
+              </li>
+              <li onClick={() => setWichCard(2)} className="nav-link">
+                QCM
+              </li>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <p>{WhichMentalCard()}</p>
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/Checkcards.tsx
+++ src/Components/Checkcards.tsx
@@ -3,77 +3,98 @@ import { gonnaBeADataBase } from "../data/data";
 
 const Checkcards = () => {
   const [currentCard, setCurrentCard] = useState(0);
-  const [userAnswer, setUserAnswer] = useState("bliblablou");
-  const [trueOrFalse, setTrueOrFalse] = useState("");
-  const [triggerUseEffect, setTriggerUseEffect] = useState(false);
-  const [uech, setUech] = [""];
-  const blblblblbl: string[] = []
+  const [shuffledQuestions, setShuffledQuestions] = useState(["", "", "", ""]);
+  const [userAnswer, setUserAnswer] = useState("");
+  const [results, setResults] = useState("");
 
-  useEffect((): void => {
+  useEffect(() => {
+    shuffleResponses();
+  }, [currentCard]);
+
+  function changeQuestion() {
+    setCurrentCard(
+      Math.floor(Math.random() * (gonnaBeADataBase.length - 1) + 1)
+    );
+  }
+
+  function shuffleResponses() {
+    const bufferArray = [
+      gonnaBeADataBase[currentCard].goodanswer,
+      gonnaBeADataBase[currentCard].answer2,
+      gonnaBeADataBase[currentCard].answer3,
+      gonnaBeADataBase[currentCard].answer4,
+    ];
+    setShuffledQuestions(bufferArray.sort((a, b) => 0.5 - Math.random()));
+  }
+
+  function isThatAGoodAnswer() {
     if (userAnswer === gonnaBeADataBase[currentCard].goodanswer) {
-      setTrueOrFalse("this is true");
-
-      console.log("ayoooo")
-    } else if (userAnswer === undefined) {
-      setTrueOrFalse("this is not true");
-      const setUech(() => blblblblbl.sort((a, b) => 0.5 - Math.random()));
+      setResults("C'est gagné");
+    } else {
+      setResults("C'est perdu");
     }
-  }, [triggerUseEffect]);
-
-  blblblblbl.push(
-    gonnaBeADataBase[currentCard].goodanswer,
-    gonnaBeADataBase[currentCard].answer2,
-    gonnaBeADataBase[currentCard].answer3,
-    gonnaBeADataBase[currentCard].answer4
-  );
+  }
 
   return (
-    <div>
-      <p
-        onClick={() =>
-          setCurrentCard(Math.floor(Math.random() * (gonnaBeADataBase.length - 1) + 1))
-        }
-      >
-        {gonnaBeADataBase[currentCard].question}
-      </p>
-      <form>
-        <div>
-          <input
-            type="checkbox"
-            value={uech[0]}
-            onChange={() => setUserAnswer(uech[0])}
-          />
-          <label>{uech[0]}</label>
+    <div className="container">
+      <div className="row align-items-start">
+        <div className="col">
+          <p></p>
         </div>
-        <div>
-          <input
-            type="checkbox"
-            value={uech[1]}
-            onChange={() => setUserAnswer(uech[1])}
-          />
-          <label>{uech[1]}</label>
+        <div className="col">
+          <div className="card text-dark bg-light mb-3">
+            <div className="card-header">QCM</div>
+            <div className="card-body">
+            <div onClick={() => changeQuestion()} className="card-title">
+              {gonnaBeADataBase[currentCard].question}
+            </div>
+              <div className="card-text">
+                <input
+                  type="checkbox"
+                  id="check1"
+                  name="check1"
+                  onClick={() => setUserAnswer(shuffledQuestions[0])}
+                />
+                <label>{shuffledQuestions[0]}</label>
+              </div>
+              <div className="card-text">
+                <input
+                  type="checkbox"
+                  id="check2"
+                  name="check2"
+                  onClick={() => setUserAnswer(shuffledQuestions[1])}
+                />
+                <label>{shuffledQuestions[1]}</label>
+              </div>
+              <div className="card-text">
+                <input
+                  type="checkbox"
+                  id="check3"
+                  name="check3"
+                  onClick={() => setUserAnswer(shuffledQuestions[2])}
+                />
+                <label>{shuffledQuestions[2]}</label>
+              </div>
+              <div className="card-text">
+                <input
+                  type="checkbox"
+                  id="check4"
+                  name="check4"
+                  onClick={() => setUserAnswer(shuffledQuestions[3])}
+                />
+                <label>{shuffledQuestions[3]}</label>
+              </div>
+              <div className="card-text">
+                <button onClick={() => isThatAGoodAnswer()}>Submit</button>
+              </div>
+              <div>{results}</div>
+            </div>
+          </div>
         </div>
-        <div>
-          <input
-            type="checkbox"
-            value={uech[2]}
-            onChange={() => setUserAnswer(uech[2])}
-          />
-          <label>{uech[2]}</label>
+        <div className="col">
+          <p></p>
         </div>
-        <div>
-          <input
-            type="checkbox"
-            value={uech[3]}
-            onChange={() => setUserAnswer(uech[3])}
-          />
-          <label>{uech[3]}</label>
-        </div>
-      </form>
-      <button onClick={() => setTriggerUseEffect(!triggerUseEffect)}>
-        Repondre
-      </button>
-      <p>{trueOrFalse}</p>
+      </div>
     </div>
   );
 };
